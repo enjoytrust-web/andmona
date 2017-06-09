df1701c001d8cbb13ff17cdc3c456aedd33746bd
ichimiya enjoytrust <ichimiya.enjoy@gmail.com>
2017-06-09 19:13:35 +0900

    静的ページ完成

--- tpl_andmona_v1.1/index.php
+++ tpl_andmona_v1.1/index.php
@@ -9,12 +9,12 @@
     <title>&amp;mona</title>
 
   <!-- StyleSheet -->
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/lib/css/style.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/lib/css/slick.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/lib/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/lib/css/slick.css">
 
     <!-- Script -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="<?php bloginfo('template_url');?>/js/slick.min.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/slick.min.js"></script>
 
   <!-- favicon -->
   <link rel="apple-touch-icon" sizes="180x180" href="">
@@ -44,22 +44,21 @@
   <link rel="author" href="">
 </head>
 
-<body>
+<body onload="initialize();">
 <!-- ▼ write your contents ▼ -->
 
   <section class="l-wrapper">
-    <div class="l-container">
+    <div class="l-container position_center">
       <div class="l-row">
         <div class="l-grid-12 padding0">
           <div class="border">
             <div class="img_mainvisual">
-              <img src="<?php bloginfo('template_url');?>/lib/image/mainvisual.png" alt="&amp;mona">
+              <img src="<?php bloginfo('template_url'); ?>/lib/image/mainvisual.png" alt="&amp;mona">
             </div>
-            <!--
             <div class="scroll">
-              <a href="#about"><span></span>Scroll</a>
+              <i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+              <p>Scroll</p>
             </div>
-            -->
           </div>
         </div>
       </div>
@@ -67,16 +66,16 @@
   </section>
 
 
-  <section class="l-wrapper">
-    <div class="l-container">
+  <section class="l-wrapper" id="about">
+    <div class="l-container position_center">
       <div class="l-row">
         <div class="l-grid-12 padding0">
-          <h1>
-            <img src="<?php bloginfo('template_url');?>/lib/image/logo.png" alt="&amp;mona">
+          <h1 class="logo">
+            <img src="<?php bloginfo('template_url'); ?>/lib/image/logo.png" alt="&amp;mona">
           </h1>
-          <h2>sports &times; music &times; fashion</h2>
-          <p>スポーツ、音楽、ファッションの３要素を融合させて独自の新しい形に...</p>
-          <p>高感度のナイトクラバーにクラブファッションとライフスタイルを提案します</p>
+          <h2 class="semititle">sports &times; music &times; fashion</h2>
+          <p class="about_text">スポーツ、音楽、ファッションの３要素を融合させて独自の新しい形に...</p>
+          <p class="about_text">高感度のナイトクラバーにクラブファッションとライフスタイルを提案します</p>
         </div>
       </div>
     </div>
@@ -84,40 +83,43 @@
 
 
   <section class="l-wrapper">
-    <div class="l-container">
+    <div class="l-container position_center">
       <div class="l-row">
-        <div class="l-grid-3 item padding0">
-          <div class="item_text">
-            <h3 class="gallery">Gallery</h3>
+        <div class="l-grid-3 padding0 item_border">
+          <div class="item_textbox">
+            <h3 class="item_text">Gallery</h3>
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item1">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item2">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item3">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item4">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item5">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <div class="img_item">
+        <div class="l-grid-3 padding0">
+          <div class="img_item item6">
           </div>
         </div>
-        <div class="l-grid-3 item padding0">
-          <a href="#" class="item_text">
-            <h3 class="gallery">See all</h3>
+        <div class="l-grid-3 padding0 item_border">
+          <a href="#" class="item_textbox">
+            <h3 class="item_text">
+              <span>See all</span>
+              <i class="fa fa-long-arrow-right arrow" aria-hidden="true"></i>
+            </h3>
           </a>
         </div>
       </div>
@@ -126,49 +128,69 @@
 
 
   <section class="l-wrapper">
-    <div class="l-container">
+    <div class="l-container position_center">
       <div class="l-row">
-        <div class="l-grid-6 padding0 height50vh">
-          <h3 class="shop_info">
-            <span>SHOP INFO</span>
-            取り扱い店舗
-          </h3>
-          <ul class="shop_info_list">
-            <li>
-              <span>SHOP NAME</span>
-              Club Ammona
-            </li>
-            <li>
-              <span>Tel</span>
-              06-6211-3993
-            </li>
-            <li>
-              <span>Adress</span>
-              〒542-0083 大阪市中央区東心斎橋2-3-22玉八ビル
-            </li>
-          </ul>
+        <div class="l-grid-6 padding0 map_wrapper">
+          <div class="shopinfo_wrapper">
+            <h3 class="shopinfo_title">
+              <span>SHOP INFO</span>
+              取り扱い店舗
+            </h3>
+            <ul class="shopinfo_discription">
+              <li>
+                <span>SHOP NAME</span>
+                Club Ammona ( http://ammona.jp/ )
+              </li>
+              <li>
+                <span>Tel</span>
+                06-6211-3993
+              </li>
+              <li>
+                <span>Adress</span>
+                〒542-0083 大阪市中央区東心斎橋2-3-22玉八ビル
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="l-grid-6 padding0">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.3607957753543!2d135.50243786523134!3d34.67084248044212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e715ba91f8a1%3A0x4302417c97c3d721!2z44CSNTQyLTAwODMg5aSn6Ziq5bqc5aSn6Ziq5biC5Lit5aSu5Yy65p2x5b-D5paO5qmL77yS5LiB55uu77yT4oiS77yS77ySIOeOieWFq-ODk-ODqw!5e0!3m2!1sja!2sjp!4v1496395752080" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div class="l-grid-6 padding0 map_wrapper">
+            <div class="googlemap" id="googlemap"></div>
         </div>
       </div>
     </div>
   </section>
 
 
-<!--<script src="js/app.js"></script>-->
-<script>
-  $(function(){
-  $('a[href^=#]').click(function(){
-    var speed = 400;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
-});
-</script>
+
+  <footer class="l-wrapper l-footer">
+    <div class="l-container">
+      <div class="l-row">
+        <div class="l-grid-12 padding0">
+          <ul class="sns">
+            <li>
+              <a href="https://twitter.com/and_mona_info">
+                <i class="fa fa-twitter sns_icon" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-facebook sns_icon" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/and_mona_/">
+                <i class="fa fa-instagram sns_icon" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+          <h4 class="copyright">&copy; 2017 &amp;mona</h4>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCJySDnXDlK5mSo9CPSOOUXvywXEo9wAGM"></script>
+  <script src="<?php bloginfo('template_url'); ?>/lib/js/map.js"></script>
 </body>
 </html>
 <?php get_footer(); ?>
