8bc7130f67c61d193ab7e6678642e5ca75314709
nakamura enjoy <nakamura@enjoytrust.net>
2017-08-03 15:07:52 +0900

    rescent commit

--- tpl_andmona_v1.1/footer.php
+++ tpl_andmona_v1.1/footer.php
@@ -1,4 +1,15 @@
-
 <?php wp_footer(); ?>
+<!-- ▼ write your contents ▼ -->
+<script src="<?php bloginfo('template_url');?>/lib/js/app.js"></script>
+
+<script>
+  $(function(){
+    $('.trigger-wrapper').click(function(){
+      $('.trigger-line').toggleClass('trigger-line-active');
+      $('.g-nav').toggleClass('g-nav-active');
+      $('.content-wrapper').toggleClass('content-wrapper-deactive');
+    });
+  });
+</script>
 </body>
 </html>
--- tpl_andmona_v1.1/header.php
+++ tpl_andmona_v1.1/header.php
@@ -1,3 +1,84 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta name="robots" content="index,follow">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title></title>
+
+  <!-- StyleSheet -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:200,400,500" rel="stylesheet">
+  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/lib/css/style.css">
+
+    <!-- Script -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="">
+  <link rel="icon" type="image/png" href="" sizes="32x32">
+  <link rel="icon" type="image/png" href="" sizes="16x16">
+
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- OGP -->
+  <!-- Facebook -->
+    <meta property="fb:admins" content="">
+    <meta property="og:url" content="">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:image" content="">
+    <meta property="og:description" content="">
+    <meta property="og:site_name" content="">
+  <!-- Twitter -->
+    <meta name="twitter:site" value="">
+    <meta name="twitter:creator" value="">
+    <meta name="twitter:card" value="">
+  <!-- Google+ -->
+    <link rel="author" href="">
+</head>
+
+<body>
+
+<header class="l-header">
+
+  <nav class="g-nav">
+    <ul class="g-nav-list">
+      <li class="g-nav-listin">
+        <a href="" class="g-nav-listin-item">
+          HOME
+        </a>
+        <!-- <a href="" class="g-nav-listin-item">
+          COLLECTION
+        </a> -->
+        <a href="" class="g-nav-listin-item">
+          NEW ARRIVAL
+        </a>
+        <a href="" class="g-nav-listin-item">
+          PICK UP
+        </a>
+        <a href="" class="g-nav-listin-item">
+          STORE INFO
+        </a>
+        <a href="" class="g-nav-listin-item">
+          CONTACT
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="trigger-wrapper">
+    <div class="trigger-line"></div>
+    <div class="trigger-line"></div>
+    <div class="trigger-line"></div>
+  </div>
+
+</header>
+
 <!-- ▼ write your contents ▼ -->
 
 <?php wp_head(); ?>
