d50d63aad51d249e80ea8bb5d8e446a1c317d904
enjoy-sakaguchi <sakaguchi@enjoytrust.net>
2017-08-08 18:55:46 +0900

    商品ページ(single.php)作成中。

--- tpl_andmona_v1.1/single.php
+++ tpl_andmona_v1.1/single.php
@@ -2,4 +2,23 @@
 
 <!-- ▼ write your contents ▼ -->
 
+<section class="products">
+
+	<div class="l-full">
+		<div class="l-offset-left-2 l-grid-8 l-offset-right-2">
+			<div class="products-text-box">
+				<p class="products-text-boxin-name">NAME</p>
+				<p class="products-text-boxin-price">￥1,000</p>
+				<p class="products-text-boxin-detail">シックでシンプルなアンモナロゴ入りコンパクトミラー
+				</p>
+				<div class="products-text-boxin-color"></div>
+				<p class="products-text-boxin-size"></p>
+			</div>
+
+			</div>
+		</div>
+	</div>
+
+</section>
+
 <?php get_footer(); ?>
