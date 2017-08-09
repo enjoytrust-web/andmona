48be7fa97f071cda7775a7cfb84ad0791b0c2131
enjoy-sakaguchi <sakaguchi@enjoytrust.net>
2017-08-09 17:22:00 +0900

    商品ページ パターン2

--- tpl_andmona_v1.1/single.php
+++ tpl_andmona_v1.1/single.php
@@ -5,38 +5,29 @@
 <section class="products">
 
 	<div class="l-full">
-		<div class="l-offset-left-2 l-grid-8 l-offset-right-2">
-			<div class="products-text-box">
-				<p class="products-text-boxin-name">NAME</p>
-				<p class="products-text-boxin-price">￥1,000</p>
-				<p class="products-text-boxin-detail">シックでシンプルなアンモナロゴ入りコンパクトミラー
-				</p>
-				<div class="products-text-boxin-detail-box">
-					<p class="products-text-boxin-detail-boxin-title">Color</p>
-					<a href="#" title="black">
-						<img class="products-text-boxin-detail-boxin-color" src="<?php bloginfo('template_url');?>/lib/image/color-black.jpg" alt="black">
-					</a>
-					<p class="products-text-boxin-detail-boxin-title">Size</p>
-					<p class="products-text-boxin-detail-boxin-size">S</p>
-				</div>
-				<p class="products-text-boxin-size"></p>
-			</div>
+		<div class="products-bg">
+			<div class="l-grid-8">
+				<img class="products-imgs" src="<?php bloginfo('template_url');?>/lib/image/item_mirror_20170702.jpg" alt="">
 			</div>
 
-			<div class="l-full">
-				<div class="l-grid-4">
-					<img class="products-imgs" src="<?php bloginfo('template_url');?>/lib/image/item_top_20170702.jpg" alt="">
-				</div>
-
-				<div class="l-grid-4">
-					<img class="products-imgs" src="<?php bloginfo('template_url');?>/lib/image/item_top_20170702.jpg" alt="">
-				</div>
-
-				<div class="l-grid-4">
-					<img class="products-imgs" src="<?php bloginfo('template_url');?>/lib/image/item_top_20170702.jpg" alt="">
+			<div class="l-offset-right-1 l-grid-3 flex-grow">
+				<div class="products-text-box">
+					<p class="products-text-boxin-name">Compact Mirror</p>
+					<p class="products-text-boxin-price">￥1,000</p>
+					<p class="products-text-boxin-detail">シックでシンプルなアンモナロゴ入りコンパクトミラー
+					</p>
+					<div class="products-text-boxin-detail-box">
+						<p class="products-text-boxin-detail-boxin-title">Color</p>
+						<a href="#" title="black">
+							<img class="products-text-boxin-detail-boxin-color" src="<?php bloginfo('template_url');?>/lib/image/color-black.jpg" alt="black">
+						</a>
+						<p class="products-text-boxin-detail-boxin-title">Size</p>
+						<p class="products-text-boxin-detail-boxin-size">S</p>
+					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
 </section>
 
