e6dd660269e9842d2f494693983c1089933cb233
enjoy-sakaguchi <sakaguchi@enjoytrust.net>
2017-08-10 16:49:45 +0900

    デテールの追加

--- tpl_andmona_v1.1/single.php
+++ tpl_andmona_v1.1/single.php
@@ -9,7 +9,6 @@
 			<div class="l-grid-8">
 				<img class="products-imgs" src="<?php bloginfo('template_url');?>/lib/image/item_mirror_20170702.jpg" alt="">
 			</div><!-- grid END -->
-
 			<div class="l-offset-right-1 l-grid-3 flex-grow">
 				<div class="products-text-box">
 					<p class="products-text-boxin-name">Compact Mirror</p>
@@ -28,8 +27,38 @@
 					<a href="contact.php" class="products-text-boxin-contact"><p class="products-text-boxin-contact-text">お問い合わせ</p></a>
 				</div><!-- products-text-box END -->
 			</div><!-- grid END -->
+
+<!-- detail START -->
+			<div class="l-offset-left-1 l-grid-3">
+				<div class="products-detail-box">
+					<dl class="products-text-box-dlist">
+						<dt class="products-text-box-dlistin-title">材質</dt>
+						<dd class="products-text-box-dlistin-detail">ABC樹脂 / 鏡（ガラス）</dd>
+
+						<dt class="products-text-box-dlistin-title">注意</dt>
+						<dd class="products-text-box-dlistin-detail">変質・変色の恐れがありますので、シンナー・アルコ―ル等の有機系溶剤が付着しないようにご注意ください。</dd>
+						<dd class="products-text-box-dlistin-detail">たわしや磨き粉などで拭きますと、ガラス面や本体に傷がつきますので使用しないで下さい。</dd>
+						<dd class="products-text-box-dlistin-detail">本体や鏡は柔らかい布地で軽く拭いてください。力を入れすぎると、キズがつく場合があります。</dd>
+						<dd class="products-text-box-dlistin-detail">落としたり硬いものにぶつかると鏡が割れて怪我をする恐れがあります。</dd>
+						<dd class="products-text-box-dlistin-detail">直射日光の当たる場所や火気のそばでの使用・保管はしないで下さい。</dd>
+						<dd class="products-text-box-dlistin-detail">本来の目的外でのご使用はお止め下さい。</dd>
+						<dd class="products-text-box-dlistin-detail">乳幼児の手の届かないところに保管して下さい。</dd>
+						<dd class="products-text-box-dlistin-detail">鏡や本体部分が破損した場合はご使用をお止め下さい。</dd>
+					</dl>
+				</div>
+			</div>
+			<div class="l-grid-4">
+				
+			</div>
+
+			<div class="l-grid-3 l-offset-right-1">
+				
+			</div>
+
 		</div><!-- products-bg END -->
 	</div><!-- l-full END -->
+
+
 
 </section>
 
