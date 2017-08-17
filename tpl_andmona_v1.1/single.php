6504b404bf041e32bb87b06f1a38ef4535444863
enjoy-sakaguchi <sakaguchi@enjoytrust.net>
2017-08-17 18:26:41 +0900

    シングルページ：一旦完成

--- tpl_andmona_v1.1/single.php
+++ tpl_andmona_v1.1/single.php
@@ -2,7 +2,7 @@
 
 <!-- ▼ write your contents ▼ -->
 
-<section class="base-wrapp">
+<section class="products-wrapp">
   <div class="l-full">
     <div class="l-grid-8">
       <img class="products-pics" src="<?php bloginfo('template_url');?>/lib/image/item_mirror_20170702.jpg" alt="Compact Mirror">
@@ -11,8 +11,8 @@
     <div class="l-grid-4">
       <div class="base-text-box">
 
-        <div class="products-text-boxin-box op-products-text-boxin-box-position-1">
-          <div class="products-text-boxin-sns-widthbox">
+        <div class="base-text-boxin-box op-products-text-boxin-box-position-1">
+          <div class="base-text-boxin-sns-widthbox">
             <p class="products-text-boxin-sns-text">SHARE ON：</p>
             <ul class="sns_btn">
                 <li class="sns_btnInner">
@@ -33,14 +33,17 @@
           </div><!-- products-text-boxin-sns-widthbox -->
         </div><!-- products-text-boxin-sns END -->
 
-        <div class="products-text-boxin-box op-products-text-boxin-box-position-2">
-          <div class="products-text-boxin-box-details-widthbox">
+        <div class="base-text-boxin-box op-products-text-boxin-box-position-2">
+          <div class="base-text-boxin-box-details-widthbox">
             <h2 class="products-text-boxin-details-name">Compact Mirror</h2>
             <div class="underborder"></div>
             <div class="make-space"></div>
             <p class="products-text-boxin-details-price">￥1,000</p>
             <div class="make-space"></div>
-            <p class="products-text-boxin-details-text">シックでシンプルなAmmonaロゴ入りコンパクトミラー</p>
+            <p class="products-text-boxin-details-text">
+            シックでシンプルなAmmonaロゴ入りコンパクトミラー<br>
+            拡大鏡付きの2面鏡。
+            </p>
             <div class="make-space"></div>
             <div class="products-text-boxin-details-box">
               <p class="products-text-boxin-details-boxin-item">Color</p>
@@ -52,7 +55,7 @@
               <div class="products-text-boxin-details-boxin-size">S</div>
             </div>
             <div class="make-space"></div>
-            <a href="#" class="products-text-boxin-contact">
+            <a href="contact.php" class="products-text-boxin-contact">
               <p class="products-text-boxin-contact-text">お問い合わせ</p>
             </a>
           </div><!-- products-text-boxin-details END -->
@@ -64,22 +67,125 @@
 </section><!-- base-wrapp END -->
 
 
-<section class="base-wrapp">
-  <div class="l-full">
-    <div class="l-grid-4">
+<section class="detail-wrapp">
+  <div class="l-full base-text-box">
+    <div class="l-grid-4 flex-order4">
       <div class="base-text-box">
-        <div class="detail-text-boxin-box op-detail-text-boxin-box-position-1">
-          <div class="detail-text-boxin-box-details-widthbox">
-            
+        <div class="base-text-boxin-box op-detail-text-boxin-box-position-1">
+          <div class="base-text-boxin-box-details-widthbox">
+            <p class="detail-text-boxin-boxin-bigtitle">DETAILS</p>
           </div>
-        </div><!-- detail-text-boxin-box END -->
+        </div><!-- base-text-boxin-box END -->
+
+        <div class="base-text-boxin-box op-detail-text-boxin-box-position-2">
+          <div class="base-text-boxin-box-details-widthbox">
+            <dl class="detail-text-boxin-list">
+              <dt class="detail-text-boxin-listin-detailname">実寸サイズ</dt>
+              <dd class="detail-text-boxin-listin-text"><span class="detail-text-boxin-listin-text-span">縦×横</span>6.5×7.5(cm)<br>
+              <span class="detail-text-boxin-listin-text-span">※素材の特性と測る位置による誤差があります。
+              </span>
+              </dd>
+
+              <div class="make-space"></div>
+
+              <dt class="detail-text-boxin-listin-detailname">材質</dt>
+              <dd class="detail-text-boxin-listin-text">ABC樹脂 / 鏡（ガラス）</dd>
+
+              <div class="make-space"></div>
+
+              <dt class="detail-text-boxin-listin-detailname">注意</dt>
+              <dd class="detail-text-boxin-listin-text">変質・変色の恐れがありますので、シンナー・アルコール等の有機系溶剤が付着しないようにご注意ください。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">たわしや磨き粉などで拭きますと、ガラス面や本体に傷がつきますので使用しないで下さい。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">本体や鏡は柔らかい布地で軽く拭いてください。力を入れすぎると、キズがつく場合があります。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">落としたり硬いものにぶつかると鏡が割れて怪我をする恐れがあります。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">直射日光の当たる場所や火気のそばでの使用・保管はしないで下さい。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">
+              本来の目的外でのご使用はお止め下さい。
+              </dd>
+              <dd class="detail-text-boxin-listin-text">
+              鏡や本体部分が破損した場合はご使用をお止め下さい。
+              </dd>
+            </dl>
+          </div>
+        </div><!-- base-text-boxin-box END -->
         </div><!-- base-text-box END -->
     </div><!-- glid END -->
 
-    <div class="l-grid-8">
+    <div class="l-grid-8 flex-order3">
       <img class="products-pics" src="<?php bloginfo('template_url');?>/lib/image/item_mirror_20170702.jpg" alt="Compact Mirror">
     </div><!-- glid END -->
   </div><!-- l-full END -->
 </section><!-- base-wrapp END -->
+
+<section class="recommend-wrapp">
+  <h2 class="section-header">
+    NEW ARRIVAL
+    <span class="section-header-jp">新作</span>
+  </h2>
+
+  <div class="l-full">
+    <div class="l-grid-3 top-new-arrival">
+      <div class="content-item-box">
+        <a href="#" class="content-item-box-link">
+          <figure class="content-item-productpics"style="background-image: url('<?php the_field('product_mainpics');?>')"></figure>
+          <h2 class="content-item-productname">
+            <?php the_field('product_name'); ?>
+            <span class="more">
+              MORE
+            </span>
+          </h2>
+        </a>
+      </div><!-- content-item-box END -->
+    </div><!-- grid END -->
+
+    <div class="l-grid-3 top-new-arrival">
+      <div class="content-item-box">
+        <a href="#" class="content-item-box-link">
+          <figure class="content-item-productpics"style="background-image: url('<?php the_field('product_mainpics');?>')"></figure>
+          <h2 class="content-item-productname">
+            <?php the_field('product_name'); ?>
+            <span class="more">
+              MORE
+            </span>
+          </h2>
+        </a>
+      </div><!-- content-item-box END -->
+    </div><!-- grid END -->
+
+    <div class="l-grid-3 top-new-arrival">
+      <div class="content-item-box">
+        <a href="#" class="content-item-box-link">
+          <figure class="content-item-productpics"style="background-image: url('<?php the_field('product_mainpics');?>')"></figure>
+          <h2 class="content-item-productname">
+            <?php the_field('product_name'); ?>
+            <span class="more">
+              MORE
+            </span>
+          </h2>
+        </a>
+      </div><!-- content-item-box END -->
+    </div><!-- grid END -->
+
+    <div class="l-grid-3 top-new-arrival">
+      <div class="content-item-box">
+        <a href="#" class="content-item-box-link">
+          <figure class="content-item-productpics"style="background-image: url('<?php the_field('product_mainpics');?>')"></figure>
+          <h2 class="content-item-productname">
+            <?php the_field('product_name'); ?>
+            <span class="more">
+              MORE
+            </span>
+          </h2>
+        </a>
+      </div><!-- content-item-box END -->
+    </div><!-- grid END -->
+  </div><!-- l-full END -->
+
+</section>
 
 <?php get_footer(); ?>
